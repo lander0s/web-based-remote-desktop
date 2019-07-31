883c75f5603385a00be3f2bfd3d62b6010704d23
David Landeros <dh.landeros08@gmail.com>
2019-07-30 23:21:02 -0700

    add initial files

---
+++ server.js
@@ -0,0 +1,15 @@
+const screenshot = require('screenshot-desktop');
+const http = require('http');
+const Jimp = require('jimp');
+
+http.createServer((request, response)=>{
+    screenshot({format:'png'}).then((originalBuffer)=>{
+        Jimp.read(originalBuffer).then((imageObj)=>{
+            imageObj.scale(0.5).quality(60)
+              .getBufferAsync(Jimp.MIME_PNG).then((resultingBuffer) => {
+                response.writeHead(200, {'content-type' : '	image/png'});
+                response.end(resultingBuffer);
+            });
+        });
+    });
+}).listen(8080);
