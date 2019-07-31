17e218c08fd937a4c6ad0b589c53ec2d321bf58b
David Landeros <dh.landeros08@gmail.com>
2019-07-30 23:55:24 -0700

    add http route to send a mouse click

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
-const screenshot = require('screenshot-desktop');
 const http = require('http');
+const screenshot = require('screenshot-desktop');
 const Jimp = require('jimp');
+const robot = require('robotjs');
+const url = require('url');
 
-http.createServer((request, response)=>{
+//robot.moveMouse(100,100);
+//robot.mouseClick();
+
+function getScreen(query, response) {
     screenshot({format:'png'}).then((originalBuffer)=>{
         Jimp.read(originalBuffer).then((imageObj)=>{
-            imageObj.scale(0.5).quality(60)
+            imageObj.scale(parseFloat(query.scale) || 0.5).quality(parseInt(query.quality) || 60)
               .getBufferAsync(Jimp.MIME_PNG).then((resultingBuffer) => {
-                response.writeHead(200, {'content-type' : 'image/png'});
-                response.end(resultingBuffer);
+                  response.writeHead(200, { 'content-type' : 'image/png' });
+                  response.end(resultingBuffer);
             });
         });
     });
+}
+
+function input(query, response) {
+    if(query.type == 'click') {
+        const screenSize = robot.getScreenSize();
+        robot.moveMouse(query.x * screenSize.width, query.y * screenSize.height);
+        robot.mouseClick();
+    }
+    response.end('ok');
+}
+
+http.createServer((request, response)=>{
+    let parsedUrl = url.parse(request.url, true);
+    console.log(parsedUrl.pathname);
+    if(parsedUrl.pathname == '/getScreen') {
+        getScreen(parsedUrl.query, response);
+    } else if(parsedUrl.pathname == '/input') {
+        input(parsedUrl.query, response);
+    }
 }).listen(8080);
